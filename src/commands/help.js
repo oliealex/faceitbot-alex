49100d62acd893cd16a2c6f75144911bbb7ccd2e
oliealex <badass_alex@hotmail.com>
2021-03-30 16:11:50 +0200

    Added latest functionality to help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
   async execute(message, args) {
     const embed = new MessageEmbed()
     .setTitle('__COMMANDS__')
-    .setDescription(`**help** - Sends this message.\n**player <Player name>** - Returns player stats.`)
+    .setDescription(`**PREFIX=**!\n**help** - Sends this message.\n**player <Player name>** - Returns player stats.\n**last <Player name>** - Returns players latest game`)
     .setThumbnail('https://pbs.twimg.com/profile_images/1349712390628270081/KpMEtOII.png')
     .setFooter("Faceit CSGO stats")
     .setColor(0xFF4500);
